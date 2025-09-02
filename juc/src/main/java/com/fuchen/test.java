1dd2cecda2237167990a91a94519c303586291f6
ysehertz <143599399+ysehertz@users.noreply.github.com>
2025-09-02 17:53:40 +0800

    创建测试类

---
+++ juc/src/main/java/com/fuchen/test.java
@@ -0,0 +1,19 @@
+package com.fuchen;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ClassName: test
+ * Package: com.fuchen
+ * Description:
+ *
+ * @author fuchen
+ * @version 1.0
+ * @createTime 2025/9/2
+ */
+@Slf4j
+public class test {
+    public static void main(String[] args) {
+        log.info("hello world");
+    }
+}
---
+++ src/main/java/com/fuchen/Main.java
@@ -0,0 +1,14 @@
+package com.fuchen;
+
+/**
+* ClassName: ${NAME}
+* Package: com.fuchen
+* Description: 
+* @author fuchen
+* @createTime 2025/8/30
+* @version 1.0   
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
